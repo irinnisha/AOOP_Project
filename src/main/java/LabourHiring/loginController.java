15f63452129100b2c1fb622566879b77dd155e76
irinnisha <71202635+irinnisha@users.noreply.github.com>
2023-06-22 01:12:22 +0600

    k

--- src/main/java/LabourHiring/loginController.java
+++ src/main/java/LabourHiring/loginController.java
@@ -1,16 +1,47 @@
 package LabourHiring;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 public class loginController {
 
     @FXML
-    private ImageView logInImage;
+    private Button CancelButton;
+
+    @FXML
+    private Button SignUp;
+
+    @FXML
+    private TextField UserPassward;
+
+    @FXML
+    private ImageView emailIcon;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private ImageView passIcon;
 
     @FXML
     private TextField userEmail;
 
+    @FXML
+    void OnActionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionLoginButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionSignupButton(ActionEvent event) {
+
+    }
 
 }
